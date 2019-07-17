8ee4ba507787685da50dc3e44a686ddb8b0471b8
vizhali <52244020+vizhali@users.noreply.github.com>
2019-07-17 10:36:00 +0530

    Create word.py

---
+++ word.py
@@ -0,0 +1,23 @@
+vizh1=int(input())
+if vizh1==1:
+    print("One")
+elif vizh1==2:
+    print("Two")
+elif vizh1==3:
+    print("Three")
+elif vizh1==4:
+    print("Four")
+elif vizh1==5:
+    print("Five")
+elif vizh1==6:
+    print("Six")
+elif vizh1==7:
+    print("seven")
+elif vizh1==8:
+    print("Eight")
+elif vizh1==9:
+    print("Nine")
+elif vizh1==10:
+    print("Ten")
+else:
+    print("no")
